d8865bc7da66151644a786ca9f46040eb44deb3e
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-05-19 10:34:44 +0800

    add high score

--- score.py
+++ score.py
@@ -16,13 +16,26 @@ class Score(Turtle):
         self.write(self.lscore, align="center", font=("Courier", 80, "normal"))
         self.goto(100, 200)
         self.write(self.rscore, align="center", font=("Courier", 80, "normal"))
+        self.goto(0, 150)
+        self.write("highest score is : " + str(self.get_highscore())+ " - 3", align="center", font=("Courier", 20, "normal"))
 
     def addlscore(self):
         self.lscore += 1
         self.update_score()
+        if(self.lscore >= self.get_highscore()):
+            self.update_highscore(self.lscore)
 
     def addrscore(self):
         self.rscore += 1
         self.update_score()
     def getmax(self):
-        return max(self.lscore, self.rscore)
+        return self.rscore
+    
+    def get_highscore(self):
+        with open("pong\score.txt") as f:
+            return int(f.read())
+
+    def update_highscore(self, score):
+        with open("pong\score.txt", "w") as f:
+            f.write(str(score))
+        self.update_score()
